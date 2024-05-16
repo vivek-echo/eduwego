b1aa4b03ccc87414d67599c28a63698da5e4741b
Vivek Ranjan. Jha <vivek.jha@corp.csmpl.com>
2024-05-17 00:17:45 +0530

    changes

--- resources/views/WebView/WebViewLayouts/WebViewLayout.blade.php
+++ resources/views/WebView/WebViewLayouts/WebViewLayout.blade.php
@@ -2,14 +2,39 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Enlight Educational Trust – Together We To Lead</title>
+
+    <!-- <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico"> -->
+    <link rel="icon" href="img/cropped-Logo-32x32.png" sizes="32x32" />
+    <link rel="icon" href="img/cropped-Logo-192x192.png" sizes="192x192" />
+    <link rel="apple-touch-icon" href="img/cropped-Logo-180x180.png" />
+    <!-- Place favicon.ico in the root directory -->
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/animate.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/aos.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/magnific-popup.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/icofont.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/slick.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/swiper-bundle.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
 </head>
 
 <body>
     <header>
-
+        <!-- pre loader area start -->
+        <div id="back__preloader">
+            <div id="back__circle_loader"></div>
+            <div class="back__loader_logo">
+                <img loading="lazy" src="{{asset('assets/img/pre.png')}}" alt="Preload">
+            </div>
+        </div>
+        <!-- pre loader area end -->
     </header>
 
     <div class="content">
@@ -18,6 +43,36 @@
 
     <footer>
 
+
+
+        <!-- JS here -->
+        <script src="{{asset('assets/js/vendor/modernizr-3.5.0.min.js')}}"></script>
+        <script src="{{asset('assets/js/vendor/jquery-3.6.0.min.js')}}"></script>
+        <script src="{{asset('assets/js/popper.min.js')}}"></script>
+        <script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
+        <script src="{{asset('assets/js/isotope.pkgd.min.js')}}"></script>
+        <script src="{{asset('assets/js/slick.min.js')}}"></script>
+        <script src="{{asset('assets/js/jquery.meanmenu.min.js')}}"></script>
+        <script src="{{asset('assets/js/ajax-form.js')}}"></script>
+        <script src="{{asset('assets/js/wow.min.js')}}"></script>
+        <script src="{{asset('assets/js/jquery.scrollUp.min.js')}}"></script>
+        <script src="{{asset('assets/js/imagesloaded.pkgd.min.js')}}"></script>
+        <script src="{{asset('assets/js/jquery.magnific-popup.min.js')}}"></script>
+        <script src="{{asset('assets/js/waypoints.min.js')}}"></script>
+        <script src="{{asset('assets/js/jquery.counterup.min.js')}}"></script>
+        <script src="{{asset('assets/js/plugins.js')}}"></script>
+        <script src="{{asset('assets/js/swiper-bundle.min.js')}}"></script>
+        <script src="{{asset('assets/js/main.js')}}"></script>
+
+        <script>
+            // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+            // if (localStorage.getItem("theme-color") === "dark" || (!("theme-color" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+            //   document.getElementById("light--to-dark-button")?.classList.add("dark--mode");
+            // }
+            // if (localStorage.getItem("theme-color") === "light") {
+            //   document.getElementById("light--to-dark-button")?.classList.remove("dark--mode");
+            // } 
+        </script>
     </footer>
 </body>
 
