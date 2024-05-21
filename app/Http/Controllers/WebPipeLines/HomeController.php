31fdfdb5bd7d768ac8a1d032da14906244aaa9e0
Vivek Ranjan. Jha <vivek.jha@corp.csmpl.com>
2024-05-22 00:09:46 +0530

    changes

--- app/Http/Controllers/WebPipeLines/HomeController.php
+++ app/Http/Controllers/WebPipeLines/HomeController.php
@@ -4,63 +4,92 @@ namespace App\Http\Controllers\WebPipeLines;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Artisan;
 class HomeController extends Controller
 {
-    public function homeIndex(){
-        $news =  DB::table('news')->where('newsPublish',2)->where('deletedFlag', 0)->get();
-        $notice =  DB::table('notice')->where('noticePublish',2)->where('deletedFlag', 0)->get();
-        $resp['news']= $news;
-        $resp['notice']= $notice;
+    public function homeIndex()
+    {
+        $cacheCommands = array(
+            'event:clear',
+            'view:clear',
+            'cache:clear',
+            'route:clear',
+            'config:clear',
+            'clear-compiled',
+            'optimize:clear'
+            // ,'config:cache'
+        );
 
-        return view('WebView.Home.Home',$resp);
+        foreach ($cacheCommands as $command) {
+            Artisan::call($command);
+        }
+        $news =  DB::table('news')->where('newsPublish', 2)->where('deletedFlag', 0)->get();
+        $notice =  DB::table('notice')->where('noticePublish', 2)->where('deletedFlag', 0)->get();
+        $resp['news'] = $news;
+        $resp['notice'] = $notice;
+
+        return view('WebView.Home.Home', $resp);
     }
-    public function nursingIndex(){
+    public function nursingIndex()
+    {
         return view('WebView.Nursing.Nursing');
     }
-    public function bscNursingIndex(){
+    public function bscNursingIndex()
+    {
         return view('WebView.Nursing.bScNursing');
     }
-    public function paramedicalsIndex(){
+    public function paramedicalsIndex()
+    {
         // resources\views\WebView\Paramedecials\Paramedicals.blade.php
         return view('WebView.Paramedicals.Paramedicals');
     }
-    public function galleryIndex(){
+    public function galleryIndex()
+    {
         return view('WebView.Gallery.Gallery');
     }
-    public function contactsIndex(){
+    public function contactsIndex()
+    {
         return view('WebView.Contacts.Contacts');
     }
-    public function facilitiesIndex(){
+    public function facilitiesIndex()
+    {
         return view('WebView.Facilities.Facilities');
     }
-    public function scholarshipIndex(){
+    public function scholarshipIndex()
+    {
         return view('WebView.Scholarship.Scholarship');
     }
-    public function sowrabhaInsIndex(){
+    public function sowrabhaInsIndex()
+    {
         return view('WebView.School.SowrabhaIns');
     }
-    public function englightInsIndex(){
+    public function englightInsIndex()
+    {
         return view('WebView.School.EnglightIns');
     }
-    public function enlightSchoolIndex(){
+    public function enlightSchoolIndex()
+    {
         return view('WebView.School.EnlightSchool');
-    }   
+    }
     //start added by deepak on 20-05-2024
-    public function MSCNursingIndex(){
+    public function MSCNursingIndex()
+    {
         return view('WebView.Nursing.MSCNursing');
     }
-    public function PbbscNursingIndex(){
+    public function PbbscNursingIndex()
+    {
         return view('WebView.Nursing.PbbscNursing');
     }
-    public function GnmNursingIndex(){
+    public function GnmNursingIndex()
+    {
         return view('WebView.Nursing.GnmNursing');
     }
-  
+
     public function downloadPdf()
     {
         // dd("ascjh bhg");
         $filePath = public_path('assets/Sowrabha-Prospectus.pdf'); // Path to your PDF file
-// dd($filePath);
+        // dd($filePath);
         if (!file_exists($filePath)) {
             abort(404, 'File not found.');
         }
