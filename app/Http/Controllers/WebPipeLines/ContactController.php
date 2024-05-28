b751e077ecda03d990dd6d7d8c3405ef1a3aacea
Vivek Ranjan. Jha <vivek.jha@corp.csmpl.com>
2024-05-29 01:18:10 +0530

    changes

--- app/Http/Controllers/WebPipeLines/ContactController.php
+++ app/Http/Controllers/WebPipeLines/ContactController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Artisan;
 use Exception;
 use App\Mail\ContactMail;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Log;
 
 class ContactController extends Controller
 {
@@ -19,17 +20,24 @@ class ContactController extends Controller
     public function submitContact()
     {
         try {
+            throw new Exception('Your error message here');
             $getData = request()->all();
-            
+
             Mail::to($getData['con_email'])->send(new ContactMail($getData));
             $status = true;
             $statusCode = 200;
             $msg = "successfully";
         } catch (Exception $e) {
-            dd($e->getMessage());
+
             $status = false;
             $statusCode = 400;
             $msg = "Something went wrong.Please try again later..";
+            Log::error("Error", [
+                'Controller' => 'ContactController',
+                'Method' => 'submitContact',
+                'Line'  => $e->getLine(),
+                'Error'  => $e->getMessage()
+            ]);
         }
         return response()->json(
             [
